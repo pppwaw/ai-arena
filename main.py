e1266ca4fe47eb20a74d0220bc8ce81e40c846e3
pppwaw <pppwaw@pppwaw.top>
2023-03-06 13:37:57 +0800

    1

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from math import sqrt
 
 import api
 
-t = -114514
-
 
 def cal_will_collide(x1, y1, vx1, vy1, x2, y2, vx2, vy2, r1, r2) -> (bool, float):
     # point1_t = (x1+vx1*t, y1+vy1*t)
     # point2_t = (x2+vx2*t, y2+vy2*t)
     # point1_t and point2_t's distance is radius1+radius2
     # (x1+vx1*t-x2-vx2*t)^2+(y1+vy1*t-y2-vy2*t)^2 = (radius1+radius2)^2
-    print(x1, y1, vx1, vy1, x2, y2, vx2, vy2, r1, r2, sep=',')
+    # print(x1, y1, vx1, vy1, x2, y2, vx2, vy2, r1, r2, sep=',')
     try:
         up1 = -vx1 * x1 + vx1 * x2 + vx2 * x1 - vx2 * x2 - vy1 * y1 + vy1 * y2 + vy2 * y1 - vy2 * y2
         up2 = sqrt(
@@ -23,24 +21,27 @@ def cal_will_collide(x1, y1, vx1, vy1, x2, y2, vx2, vy2, r1, r2) -> (bool, float
 
     except:
         return False, 0
-    print("cal", t1, t2)
+    # print("cal", t1, t2)
     if math.isnan(t1) or math.isnan(t2):
         return False, 0
     if t1 < -2 and t2 < -2:
         return False, 0
     if t1 < 0:
         tt = t2
+    elif t2 < 0:
+        tt = t1
     else:
         tt = min(t1, t2)
     return True, tt
 
 
 def cal_shouyi(mass, coll_list) -> float:
-    shouyi = 0
+    shouyi = mass - mass * api.SHOOT_AREA_RATIO
     for i in coll_list:
         if mass <= i.mass:
             return -mass
         shouyi += i.mass
+    shouyi -= mass
     return shouyi
 
 
@@ -50,15 +51,12 @@ def get_coll_list(context, x, y):
     for j in context.monsters + context.other_players + context.npc:
         if (x + j.vx) ** 2 + (y + j.vy) ** 2 < 0.1:
             continue
-        cal = cal_will_collide(context.me.x, context.me.y, x, y, j.x, j.y, j.vx, j.vy, context.me.radius, j.radius)
-        print(cal)
-        if cal[0]:
+        cal, t = cal_will_collide(context.me.x, context.me.y, x, y, j.x, j.y, j.vx, j.vy, context.me.radius, j.radius)
+        if cal:
             coll_list.append(j)
-            sum_col += cal[1]
-    coll_list.sort(key=lambda atom: math.sqrt((context.me.x + x - atom.x - atom.vx) ** 2 + (
-            context.me.y + y - atom.y - atom.vy) ** 2))
-    if sum_col == 0:
-        sum_col = 0.01
+            sum_col += t
+    coll_list.sort(key=lambda atom: math.sqrt((context.me.x - atom.x) ** 2 + (
+            context.me.y - atom.y) ** 2))
 
     return coll_list, sum_col
 
@@ -74,12 +72,12 @@ def update(context: api.RawContext):
     else:
         best_shouyi = 0
     best_rad = -114
-    print("no coll_list", coll_list)
+    print("no coll_list", [(i.x, i.y, i.vx, i.vy, i.type, i.mass) for i in coll_list])
     print("no", best_shouyi)
     angles = []
     for i in context.monsters + context.other_players + context.npc:
         angle = api.relative_angle(context.me.x, context.me.y, i.x, i.y, 0)
-        angles += [angle, angle + 180, angle + 90, angle - 90]
+        angles += [(angle + 180) % 360, angle % 360, (angle + 90) % 360, (angle - 90) % 360]
     for i in angles:
         radian = api.angle_to_radian(i)
         size = context.me.mass * api.SHOOT_AREA_RATIO
@@ -89,12 +87,11 @@ def update(context: api.RawContext):
             shouyi = cal_shouyi(context.me.mass, coll_list) + 10 / sum_col
         else:
             shouyi = 0
-        print(i, "coll_list", coll_list)
+        print("no coll_list", [(i.x, i.y, i.vx, i.vy, i.type, i.mass) for i in coll_list])
         print(i, "shouyi", shouyi)
         if shouyi > best_shouyi:
             best_shouyi = cal_shouyi(context.me.mass - size, coll_list)
             best_rad = radian
-    best_rad = best_rad.normalize()
     print("best", best_rad, best_shouyi)
     if best_rad != -114:
         return best_rad
