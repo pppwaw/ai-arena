d38425ff1abe0bfb4ae865ea7a7f8d209ae22844
pppwaw <pppwaw@pppwaw.top>
2023-05-09 18:05:44 +0800

    骨架

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ data = namedtuple("data", ["is_space", "data"])
 step = ""
 q = Queue()
 spaces = 0
+Atom = namedtuple("Atom", ["x", "y", "vx", "vy", "r", "mass"])
+
+cal = lambda x1, vx1, x2, vx2, y1, vy1, y2, vy2, r1, r2, t: \
+    (x1 + vx1 * t - x2 - vx2 * t) ** 2 + (y1 + vy1 * t - y2 - vy2 * t) ** 2 - (r1 + r2) ** 2
+
+
+def will_coll(atom1: Atom, atom2: Atom):
+    # (x1+vx1t-x2-vx2t)^2+(y1+vy1t-y2-vy2t)^2-(r1+r2)^2=0
+
+    min_t = 0
+    max_t = 100
+    while ()
 
 
 def handle_shanbi(context: api.RawContext):
@@ -16,6 +28,7 @@ def handle_shanbi(context: api.RawContext):
 
 
 def handle_target(context: api.RawContext):
+    pass
 
 
 def handler(context: api.RawContext):
